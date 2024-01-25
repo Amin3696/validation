9e3215b6782ee98eba2c3ccf55aa175b688facdf
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2024-01-25 15:56:20 +0100

    add postalCode

--- src/main/java/com/springdemo/mvc/model/Customer.java
+++ src/main/java/com/springdemo/mvc/model/Customer.java
@@ -1,7 +1,6 @@
 package com.springdemo.mvc.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +14,12 @@ public class Customer {
     @NotNull()
     @Size(min = 1, message = "is required!")
     private String lastName;
+    @Min(0)
+    @Max(10)
+    @NotNull
+    private Integer freePass;
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -28,7 +33,24 @@ public class Customer {
         return lastName;
     }
 
+    public Integer getFreePass() {
+        return freePass;
+    }
+
+    public void setFreePass(Integer freePass) {
+        this.freePass = freePass;
+    }
+
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
