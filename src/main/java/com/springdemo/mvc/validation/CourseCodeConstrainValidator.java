7d46546c888d4e054859ade683697cd9d3ece1c0
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2024-01-27 22:09:11 +0100

    add null check

--- src/main/java/com/springdemo/mvc/validation/CourseCodeConstrainValidator.java
+++ src/main/java/com/springdemo/mvc/validation/CourseCodeConstrainValidator.java
@@ -15,7 +15,13 @@ public class CourseCodeConstrainValidator implements ConstraintValidator<CourseC
 
     @Override
     public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
-
-        return theCode.startsWith(coursePrefix);
+        boolean result;
+        if (theCode != null) {
+            result = theCode.startsWith(coursePrefix);
+        }
+        else {
+            result = true;
+        }
+        return result;
     }
 }
